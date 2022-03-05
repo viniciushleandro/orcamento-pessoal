902d778f7d308276c2392d29c1de3447574f0d8e
Vinicius Leandro <vinilean43@gmail.com>
2022-03-04 22:19:20 -0300

    criando índices dinâmicos

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d) {
+        
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }
+}
+
+
+let bd = new Bd()
+
+
 function cadastrarDespesa() {
     
     let ano = document.getElementById('ano')
@@ -7,6 +47,13 @@ function cadastrarDespesa() {
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
 
-    console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+    let despesa = new Despesa(ano.value,
+        mes.value,
+        dia.value,
+        tipo.value,
+        descricao.value,
+        valor.value)
+
+    bd.gravar(despesa)
     
 }
