723245e493956357cfbd9cd0b24005a082fde7f2
Vinicius Leandro <vinilean43@gmail.com>
2022-03-06 10:33:32 -0300

    Listando despesas
    Incluindo despesas dinamicamente no HTML

--- app.js
+++ app.js
@@ -41,6 +41,33 @@ class Bd {
 
         localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistros() {
+        //array de despesas
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        //recuperar todas as despesas cadastradas em localStorage
+        for(let i = 1; i <= id; i++) {
+
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //existe possibilidade de haver índices que foram pulados/removidos
+            //nesse caso vamos pular os índices
+
+            if(despesa === null) {
+                continue
+                
+            }
+            
+            despesas.push(despesa)
+            
+        }
+
+        return despesas
+    }
 }
 
 
@@ -65,7 +92,7 @@ function cadastrarDespesa() {
 
 
     if(despesa.validarDados()) {
-        //bd.gravar(despesa)
+        bd.gravar(despesa)
 
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
         document.getElementById('modal_titulo_div').className = 'modal-header text-success'
@@ -88,6 +115,44 @@ function cadastrarDespesa() {
         $('#modalRegistraDespesa').modal('show')
     }
 
-    
-    
+}
+
+function carregaListaDespesas() {
+
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+
+    // selecionando o elemento tbody
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    //percorrer o array despesas, listando cada despesa de forma dinamica
+    despesas.forEach(function(d) {
+
+        console.log(d)
+
+        //criando a linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        //criar as colunas (td)
+        linha.insertCell(0).innerHTML = `0${d.dia}/0${d.mes}/${d.ano}`
+
+        switch(parseInt(d.tipo)) {
+            case 1: d.tipo = 'Alimentação'
+                break
+            case 2: d.tipo = 'Educação'
+                break
+            case 3: d.tipo = 'Lazer'
+                break
+            case 4: d.tipo = 'Saúde'
+                break
+            case 5: d.tipo = 'Transporte'
+                break
+        }
+
+        linha.insertCell(1).innerHTML = d.tipo
+
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
